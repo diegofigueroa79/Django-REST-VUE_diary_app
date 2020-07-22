9a86d430703d8de4667ed0264641e216853544dd
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-07-22 16:28:07 -0700

    created GET REST view

--- journal/diary/views.py
+++ journal/diary/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Entry
+from .serializers import EntrySerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def index(request):
-    context = {}
-    return render(request, 'diary/index.html', context)
+
+    if request.is_ajax():
+        if request.method == 'GET':
+            entries = Entry.objects.all()
+            # let's serialize our entries
+            serializer = EntrySerializer(entries, many=True)
+            # our serializer object has the data attribute which
+            # is the serialized data from our db
+            return Response(serializer.data)
+    
+    return render(request, 'diary/index.html')
