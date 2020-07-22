a73677b533071623df7c9910aee7df8742e2d1c3
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-07-22 10:58:57 -0700

    created Entry model

--- journal/diary/models.py
+++ journal/diary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=65)
+    text = models.TextField(max_length=255)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
