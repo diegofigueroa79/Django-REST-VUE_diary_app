6b0f1f92482cc759587fcebb96658158db1095de
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-07-27 13:09:59 -0700

    added url for getting entry detail

--- journal/diary/urls.py
+++ journal/diary/urls.py
@@ -5,4 +5,5 @@ app_name='diary'
 urlpatterns = [
     path('', views.index, name='index'),
     path('entry/', views.add_entry, name='add'),
+    path('entry/<int:entryId>/', views.edit_entry, name='edit'),
 ]
