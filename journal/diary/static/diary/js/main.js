1456d63c85290258d37f3a39f14a3986b0c5b1fc
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-07-27 15:49:07 -0700

    removed index.html links to entry.html in order to do SPA

--- journal/diary/static/diary/js/main.js
+++ journal/diary/static/diary/js/main.js
@@ -1,3 +1,14 @@
+
+const app = new Vue({
+    el: '#app',
+    delimiters: ["[[", "]]"],
+    data: {
+        entry: '',
+        form: false,
+    }
+})
+
+
 $(document).ready(function(){
     $(".list-group").on({
         mouseenter: function() {
